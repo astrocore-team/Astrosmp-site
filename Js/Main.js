2bf6e7b4aff13998fab24d5920c6c435a614e9ca
JSman225 <84781179+karl-leonardi@users.noreply.github.com>
2022-06-09 11:46:10 -0400

    added tons of shit :)

--- Js/Main.js
+++ Js/Main.js
@@ -2,7 +2,7 @@ function changeCss () {
 
     this.scrollY > 5 ? document.getElementById("header").style.opacity = ".85" : document.getElementById("header").style.opacity = "0";
     this.scrollY > 5 ? temp = true : temp = false;
-    this.scrollY > 5 ? document.getElementById("header1").style.height = "10.2%" : document.getElementById("header1").style.height = "15%";
+    this.scrollY > 5 ? document.getElementById("header1").style.height = "13.2%" : document.getElementById("header1").style.height = "18%";
   
   }
   window.addEventListener("scroll", changeCss , false);
