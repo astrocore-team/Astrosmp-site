79d1f3cd22d66753da409ea9d7c4d6eb820f887e
JSman225 <84781179+karl-leonardi@users.noreply.github.com>
2022-06-10 14:05:22 -0400

    removed navbar at the top, also added a functioning home button

--- Js/Main.js
+++ Js/Main.js
@@ -1,12 +1,3 @@
-function changeCss () {
-
-    this.scrollY > 5 ? document.getElementById("header").style.opacity = ".85" : document.getElementById("header").style.opacity = "0";
-    this.scrollY > 5 ? temp = true : temp = false;
-    this.scrollY > 5 ? document.getElementById("header1").style.height = "13.2%" : document.getElementById("header1").style.height = "18%";
-  
-  }
-  window.addEventListener("scroll", changeCss , false);
-
   async function serverCount(url){
     const response = await fetch(url);
     var data = await response.json();
