84a974fd91de2ab3f93b54136bcd0dcb1ace5149
JSman225 <kmleonardi@kmail.org>
2022-06-24 10:41:53 -0400

    removed bad code and added new image

--- m/js/header.js
+++ m/js/header.js
@@ -1,8 +1,3 @@
-// First we get the viewport height and we multiple it by 1% to get a value for a vh unit
-let vh = window.innerHeight * 0.01;
-// Then we set the value in the --vh custom property to the root of the document
-document.body.style.setProperty('--vh', `${vh}px`);
-
 var open = false;
 var temp = false;
 var keys = {37: 1, 38: 1, 39: 1, 40: 1};
